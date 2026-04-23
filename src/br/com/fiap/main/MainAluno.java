07d77058273a0b8dbc038bc5bb8ed52bb605c60e
murilo-a-souza <sir.murilos@gmail.com>
2026-04-23 12:59:21 -0300

    Ajuste dos dois objetos que seam colocados com construtores vazios pelo usuário.

--- src/br/com/fiap/main/MainAluno.java
+++ src/br/com/fiap/main/MainAluno.java
@@ -12,23 +12,31 @@ public class MainAluno {
         DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate dataAtual = LocalDate.now();
         Aluno aluno1, aluno2, aluno3, aluno4;
+        String auxiliar, nome, nascimento;
+        int rm;
 
         // Instanciando dois objetos com construtor com passagem de parametros
         // Eles são os dois objetos que recebem nossos valores reais
         aluno1 = new Aluno(573977, "Murilo Almeida Rodrigues de Souza", LocalDate.parse("20/02/2008", formatador));
         aluno2 = new Aluno(573620, "Henrique Bonachela de Carvalho Carabante", LocalDate.parse("20/02/2008", formatador));
 
-        // Objetos com entrada do usuário
+        // Objetos com entrada do usuário e construtores vazios
         try {
-            int rm3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o RM do aluno 3:"));
-            String nome3 = JOptionPane.showInputDialog("Digite o nome completo do aluno 3:");
-            LocalDate nascimento3 = lerData("Digite a data de nascimento do aluno 3 (dd/MM/yyyy):", formatador);
-            aluno3 = new Aluno(rm3, nome3, nascimento3);
+            aluno3 = new Aluno();
+            auxiliar = JOptionPane.showInputDialog("Digite o RM do aluno 3:");
+            aluno3.setRegistroMatricula(rm = Integer.parseInt(auxiliar));
+            nome = JOptionPane.showInputDialog("Digite o nome completo do aluno 3:");
+            aluno3.setNomeCompleto(nome);
+            nascimento = JOptionPane.showInputDialog("Digite a data de nascimento do aluno 3 (dd/MM/yyyy):");
+            aluno3.setDataDeNascimento(LocalDate.parse(nascimento, formatador));
 
-            int rm4 = Integer.parseInt(JOptionPane.showInputDialog("Digite o RM do aluno 4:"));
-            String nome4 = JOptionPane.showInputDialog("Digite o nome completo do aluno 4:");
-            LocalDate nascimento4 = lerData("Digite a data de nascimento do aluno 4 (dd/MM/yyyy):", formatador);
-            aluno4 = new Aluno(rm4, nome4, nascimento4);
+            aluno4 = new Aluno();
+            auxiliar = JOptionPane.showInputDialog("Digite o RM do aluno 4:");
+            aluno4.setRegistroMatricula(rm = Integer.parseInt(auxiliar));
+            nome = JOptionPane.showInputDialog("Digite o nome completo do aluno 4:");
+            aluno4.setNomeCompleto(nome);
+            nascimento = JOptionPane.showInputDialog("Digite a data de nascimento do aluno 4 (dd/MM/yyyy):");
+            aluno4.setDataDeNascimento(LocalDate.parse(nascimento, formatador));
 
             String mensagem = String.format(
                     "Data atual: %s%n%n" +
@@ -46,15 +54,7 @@ public class MainAluno {
             JOptionPane.showMessageDialog(null, mensagem, "Dados dos Alunos", JOptionPane.INFORMATION_MESSAGE);
 
         } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null,
-                    "O RM deve conter apenas números inteiros.",
-                    "Erro",
-                    JOptionPane.ERROR_MESSAGE);
-        } catch (IllegalArgumentException e) {
-            JOptionPane.showMessageDialog(null,
-                    e.getMessage(),
-                    "Erro de validação",
-                    JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
 }
